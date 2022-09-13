5d2fabeaf8f25aaa8fce643501a6b43d3d5d26b5
MadhuSaini22 <madhusaini46810@gmail.com>
2022-09-13 17:22:32 +0530

    initial commit

--- firebase.js
+++ firebase.js
@@ -1,17 +1,24 @@
 import Firebase from "Firebase/compat/app";
 import "Firebase/compat/auth";
-import { FIREBASE_API_KEY, FIREBASE_APP_ID, FIREBASE_AUTH_DOMAIN, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET } from "./Config.js";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_APP_ID,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+} from "./Config.js";
 
 const FirebaseCredentials = {
-  apiKey:  FIREBASE_API_KEY,
-  authDomain:  FIREBASE_AUTH_DOMAIN,
-  projectId:  FIREBASE_PROJECT_ID,
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
   storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId:  FIREBASE_MESSAGING_SENDER_ID,
-  appId:  FIREBASE_APP_ID,
-}
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+};
 if (!Firebase.apps.length) {
-    Firebase.initializeApp(FirebaseCredentials)
-  }
-  
-  export default Firebase;
+  Firebase.initializeApp(FirebaseCredentials);
+}
+
+export { Firebase };
--- src/contexts/useFirebaseAuth.js
+++ src/contexts/useFirebaseAuth.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Firebase from "../../Firebase";
+import { Firebase } from "../../Firebase";
 
 const formatAuthUser = (user) => ({
   uid: user.uid,
